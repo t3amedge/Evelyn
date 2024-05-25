242f79744d7c5b485dd1fdaefb8d63383eb8330a
scrappie <notscrappie@proton.me>
2024-05-25 17:24:03 +0300

    feat(utils): added error embed

--- src/utils/embeds.ts
+++ src/utils/embeds.ts
@@ -4,3 +4,8 @@ export function BaseEmbed() {
     const embed = new EmbedBuilder().setColor('Blurple').setTimestamp();
     return embed;
 }
+
+export function ErrorEmbed() {
+    const embed = new EmbedBuilder().setColor('Red').setTimestamp();
+    return embed;
+}
