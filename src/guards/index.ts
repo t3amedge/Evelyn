994f6f19e7e3f15c9422a444b50703f1d0c40ac6
scrappie <notscrappie@proton.me>
2024-06-25 17:02:24 +0300

    feat: added a path for guards

---
+++ src/guards/index.ts
@@ -0,0 +1,8 @@
+import { hasQueue } from "./hasQueue";
+import { isBlacklisted } from "./isBlacklisted";
+import { isInsideVoiceChannel } from "./isInsideVoiceChannel";
+import { isPlaying } from "./isPlaying";
+import { playerExists } from "./playerExists";
+import { volumeControl } from "./volumeControl";
+
+export { hasQueue, isBlacklisted, isInsideVoiceChannel, isPlaying, playerExists, volumeControl };
